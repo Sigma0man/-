430aa00742569d0e1633a5ed8f3fd4aa5751d629
Sigma0man <144617381+Sigma0man@users.noreply.github.com>
2023-12-18 09:29:49 +0300

    Update 3.jl

--- 3.jl
+++ 3.jl
@@ -1,58 +1,48 @@
 using HorizonSideRobots
-HSR = HorizonSideRobots
-robot = Robot("3.sit";animate=true)
-function vozvrat!(robot)
-    vniz=0
-    vlevo=0
-    while !isborder(robot,Sud)
-        vniz=vniz+1
-        move!(robot,Sud)
-    end
-    while !isborder(robot,West)
-        vlevo=vlevo+1
-        move!(robot,West)
-    end
-    return vniz,vlevo
-end
-function inverse!(Side)
-    if Side==Nord
-        Side=Sud
-    elseif Side==Sud
-        Side=Nord
-    elseif Side==West
-        Side=Ost
-    else
-        Side=West
-    return Side
-    end
-end
-function zakrasit!(robot)
-    n,l=vozvrat!(robot)
-    Side=Nord
-    while !isborder(robot,Ost)
-        if !isborder(robot,Side)
-            putmarker!(robot)
-            move!(robot,Side)
+r = Robot("3.sit",animate=true)
+function fill(r::Robot)
+    num_hor = count_steps(r,West)
+    num_ver = count_steps(r,Sud)
+    direction = Ost
+    while isborder(r,Nord)==false||isborder(r,Ost)==false
+        mark_row(r,direction)
+        if isborder(r,Nord)&&isborder(r,Ost)
+            break
         else
-            putmarker!(robot)
-            move!(robot,Ost)
-            Side=inverse!(Side)
+            move!(r,Nord)
+            direction=inverse(direction)
         end
     end
-    while !isborder(robot,Sud)
-        putmarker!(robot)
-        move!(robot,Sud)
+    to_start(r,num_ver,num_hor)
+end
+function count_steps(r::Robot,side::HorizonSide)
+    num_steps=0
+    while isborder(r,side)==false
+        num_steps+=1
+        move!(r,side)
     end
-    while !isborder(robot,West)
-        putmarker!(robot)
-        move!(robot,West)
+    return num_steps
+end
+function to_start(r::Robot,ver::Int64,hor::Int64)
+    while isborder(r,Sud)==false
+        move!(r,Sud)
     end
-    for i in  0:n-1
-        move!(robot,Nord)
+    while isborder(r,West)==false
+        move!(r,West)
     end
-    for ii in  0:l-1
-        move!(robot,Ost)
+    for i in 1:ver
+        move!(r,Nord)
+    end
+    for i in 1:hor
+        move!(r,Ost)
     end
 end
-zakrasit!(robot)
-sleep(5)
+function mark_row(r::Robot,side::HorizonSide)
+    while !isborder(r,side)
+        putmarker!(r)
+        move!(r,side)
+    end
+    putmarker!(r)
+end
+inverse(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side)+2, 4))
+fill(r)
